ad0c35a42bb5ada43825c29ff1330a48295f176c
Tha-github <thassiaaminervino@gmail.com>
2024-11-01 02:12:31 -0300

    pré finalização

--- js/custom.js
+++ js/custom.js
@@ -58,3 +58,17 @@ function myMap() {
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
+
+// Portfolio filter
+var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-item',
+    layoutMode: 'fitRows'
+});
+
+$('#portfolio-filter li').on('click', function () {
+    $("#portfolio-filter li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+    portfolioIsotope.isotope({filter: $(this).data('filter')});
+});
+
+})(jQuery);
